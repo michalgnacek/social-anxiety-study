c6928bd2a0a81816eba1050008b8b738ee14e319
Michal Gnacek <michal.gnacek@emteqlabs.com>
2022-11-20 23:17:10 +0000

    Initial commit

---
+++ Social Anxiety Study/Assets/Samples/EmteqSdk/2.0.0-exp.4/Emteq/Widgets/DataViewer/DataViewer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using EmteqLabs.Faceplate;
+using EmteqLabs.MaskProtocol;
+using UnityEngine;
+
+namespace EmteqLabs
+{
+    public class DataViewer : MonoBehaviour
+    {
+        [SerializeField]
+        private List<SensorGUIObject> _sensors;
+
+        private Dictionary<MuscleMapping, ushort> _emgAmplitudeRms;
+
+        private void Start()
+        {
+            EmteqManager.OnSensorContactStateChange += OnSensorContactStateChange;
+        }
+
+        void Update()
+        {
+            _emgAmplitudeRms = EmteqManager.GetEmgAmplitudeRms();
+            foreach(SensorGUIObject sensor in _sensors)
+            {
+                sensor.SetSensorValue(_emgAmplitudeRms[sensor.SensorName]);
+            }
+        }
+
+        private void OnDisable()
+        {
+            EmteqManager.OnSensorContactStateChange -= OnSensorContactStateChange;
+        }
+        
+        private void OnSensorContactStateChange(Dictionary<MuscleMapping, ContactState> sensorcontactstate)
+        {
+            foreach(SensorGUIObject sensor in _sensors)
+            {
+                sensor.SetContactState(sensor.SensorName, sensorcontactstate[sensor.SensorName]);
+            }
+        }
+
+        void SetVisible(bool value)
+        {
+            this.gameObject.SetActive(value);
+        }
+    }
+}
